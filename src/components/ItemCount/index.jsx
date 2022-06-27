17a2631e4147bd433b8b4aea41696cb3cc38a7be
macarena-zamar <mmacarenazamar@gmail.com>
2022-06-27 15:18:30 -0300

    Commit clase 5 Crea Contador con Botón (tiene errores)

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+
+const ItemCount = ({handleAdd, initialStock}) => {
+    const [count, setCount]= useState(0)
+    const onAdd = () => {
+
+    }
+    const onDecrement = () => {
+
+    }
+  return (
+    <div>
+        <p>
+            {count}
+        </p>
+        <button onClick={onAdd}>+</button>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={handelAdd}>Agregar al carrito</button>
+        </div>
+  )
+}
+
+export default ItemCount
--- src/conteiners/ItemListContainer/index.jsx
+++ src/conteiners/ItemListContainer/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ItemCount from '../../components/ItemCount'
 
 const ItemListContainer = ({greeting}) => {
+  const handleAdd = () => {
+    console.log("Se agrego al carrito");
+  }
   return (
     <div>
         <p>{greeting}</p>
+        <ItemCount handleAdd={handleAdd} initialStock={10}/>
     </div>
   )
 }
