a91e6820abcd5d4d63c1a8681199eeceefa33493
macarena-zamar <mmacarenazamar@gmail.com>
2022-06-27 16:31:34 -0300

    Commit clase 5 Crea contador con botón (con error)

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,20 +1,20 @@
 import React, {useState} from 'react'
 
 const ItemCount = ({handleAdd, initialStock}) => {
-    const [count, setCount]= useState(0)
+    const [count, setCount] = useState (0);
     const onAdd = () => {
-
+      setCount(count + 1);
     }
     const onDecrement = () => {
+      setCount(count - 1);
 
     }
   return (
     <div>
-        <p>
-            {count}
-        </p>
+        <p>{count}</p>
         <button onClick={onAdd}>+</button>
         <button onClick={onDecrement}>-</button>
+        <p>Cantidad de prodctos: {count}</p>
         <button onClick={handelAdd}>Agregar al carrito</button>
         </div>
   )
