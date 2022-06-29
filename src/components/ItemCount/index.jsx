2d65db822687508f5048ad3745dd2aca83f0242b
macarena-zamar <mmacarenazamar@gmail.com>
2022-06-29 11:30:49 -0300

    Commit para corregir errores y consultas

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -2,20 +2,28 @@ import React, {useState} from 'react'
 
 const ItemCount = ({handleAdd, initialStock}) => {
     const [count, setCount] = useState (0);
+    //Agregamos stock
+    let stock = 10;
     const onAdd = () => {
-      setCount(count + 1);
+      if (count < stock){
+        setCount(count + 1);
+      } else {
+        alert ("No hay mas stock");
+      }
     }
     const onDecrement = () => {
       setCount(count - 1);
-
     }
+    //quiero que al apretar el boton agrear al carrito me redirija a la pagína donde voy a tener todo el carrito de compra, es decir, las compras pre seleccionadas, lo vemos a esto más adelante?
   return (
     <div>
         <p>{count}</p>
-        <button onClick={onAdd}>+</button>
         <button onClick={onDecrement}>-</button>
+        <button>Agregar al carrito</button>
+        <button onClick={onAdd}>+</button>
+        
+        
         <p>Cantidad de prodctos: {count}</p>
-        <button onClick={handelAdd}>Agregar al carrito</button>
         </div>
   )
 }
